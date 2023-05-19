43a3bec4e588b0a0248ca70220ba85e2c80ce940
hwatanabe <tufs.h.watanabe@gmail.com>
2023-05-19 14:31:21 +0900

    Done Data type in general_concepts

---
+++ general_concepts/NOTE.md
@@ -0,0 +1,5 @@
+- The diff between u32 and i32
+  - u represents "unsigned integer" that has no +-
+  - while i represents "(signed) integer" that does
+- f32
+  - f represents "floating" that has floating-point numbers
--- general_concepts/src/main.rs
+++ general_concepts/src/main.rs
@@ -1,4 +1,5 @@
 fn main() {
+    /*** Variables and code readability ***/
     /* mutable/ immutable variables */
     let mut a: &str = "aaaaa";
     println!("before re-substitution, a is: {}", a);
@@ -20,4 +21,67 @@ fn main() {
     /* constants */
     const MAX_NUMBER: i32 = 100_000;
     println!("MAX_NUMBER is: {}", MAX_NUMBER);
+
+    /*** Data type ***/
+    /* Scholar types */
+    /* Integer */
+    let x: i32 = -1;
+    println!("i32 x is: {}", x);
+    let x: u32 = 1;
+    println!("u32 x is: {}", x);
+    /* Floating-point number */
+    let x: f64 = 2.0;
+    println!("f64 x is: {}", x);
+    /* Boolean */
+    let t: bool = true;
+    println!("t is: {}", t);
+    let f: bool = false;
+    println!("f is: {}", f);
+    /* Character */
+    // use single quotation!!
+    let c: char = '😎';
+    println!("c is: {}", c);
+    /* Tuple */
+    let tup: (i32, f64, i32) = (500, 6.4, 1);
+    let (x, y, z) = tup;
+    println!("tup: {}, {}, {}", x, y, z);
+    println!(
+        "tup (access each elem with period): {}, {}, {}",
+        tup.0, tup.1, tup.2
+    );
+    /* Array */
+    // is mainly used when you want data to be in stack rather than heap, or it has its fixed length
+    let arr: [i32; 5] = [1, 2, 3, 4, 5];
+    println!("arr[0]: {}", arr[0]);
+    let months: [&str; 12] = [
+        "January",
+        "February",
+        "March",
+        "April",
+        "May",
+        "June",
+        "July",
+        "August",
+        "September",
+        "October",
+        "November",
+        "December",
+    ];
+    println!("months[0]: {}", months[0]);
+
+    /*** Arithmetic operation ***/
+    let sum: i32 = 5 + 10;
+    println!("sum: {}", sum);
+    let diff: f64 = 95.5 - 4.3;
+    println!("diff: {}", diff);
+    let product: i32 = 4 * 30;
+    println!("product: {}", product);
+    let quotient: f64 = 56.7 / 32.2;
+    println!("quotient: {}", quotient);
+    let floored: i32 = 2 / 3;
+    println!("floored: {}", floored);
+    let remainder: i32 = 43 % 5;
+    println!("remainder: {}", remainder);
+
+    /*** Function ***/
 }
