535ee669901a1283433d2071fd2b8b2c6e88cf2d
hwatanabe <tufs.h.watanabe@gmail.com>
2023-05-23 13:38:08 +0900

    Done enumerator

--- enumerator/src/main.rs
+++ enumerator/src/main.rs
@@ -1,4 +1,7 @@
 #![allow(unused)]
+
+use rand::Rng;
+
 /* Enum definition */
 // Enumerates all possible variants and works like a namespace
 
@@ -33,7 +36,7 @@ enum Message {
     Write(String),
     ChangeColor(i32, i32, i32),
 }
-// The above does almost the same as this: (but in this case, each has its own type, which makes it much more complicated and harder to handle)
+// The above does almost the same as this: (but in the below case, each has its own type, which makes it much more complicated and harder to handle)
 // struct QuitMessage;
 // struct MoveMessage {
 //     x: i32,
@@ -47,6 +50,19 @@ impl Message {
     fn call(&self) {
         println!("Hi! Are you calling me?: {:?}", &self);
     }
+}
+
+enum Coin {
+    Penny,
+    Nickel,
+    Dime,
+    Quarter(UsState),
+}
+
+#[derive(Debug)]
+enum UsState {
+    Alabama,
+    Alaska,
 }
 
 fn main() {
@@ -90,9 +106,70 @@ fn main() {
         // meaning they are different in types each other therefore type checking and converting Option<T> to T is necessary in advance to perform the operation
         // The very process ensures its null safety before using it
         // let sum = x + y;
+
+        let five: Option<i32> = Some(5);
+        let six: Option<i32> = plus_one(five);
+        let none: Option<i32> = plus_one(None);
+        println!("six: {:?}", six);
+        println!("none: {:?}", none);
     }
+    /* match's catch-all and the _ placeholder */
+    // enhance its exhaustiveness
+    {
+        let dice_roll: i32 = rand::thread_rng().gen_range(1..7);
+        println!("dice_roll: {}", dice_roll);
+        match dice_roll {
+            1 => {
+                println!("You got: {}", 1);
+                // This tuple returns nothing
+                ()
+            }
+            2 => {
+                println!("You got: {}", 2);
+                ()
+            }
+            // The _ placeholder is similar to the catch-all but doesn't bind to that value
+            _ => (),
+        }
+    }
+    /* if let, a concise control flow */
+    // When handling only a few variants, match is a bit verbose, so use if let instead
+    // less typing, less indentation, and less boilerplate code, but without the exhaustive checking that match enforces
+    // decide which one to use depending on a situation, being aware of the trade-off between conciseness and exhaustiveness
+    let config_max: Option<u8> = Some(3u8);
+    // if config_max is of Option<u8> type,
+    if let Some(max) = config_max {
+        println!("The maximum is configured to be {}", max);
+    }
+    // Instead of going like this:
+    // match config_max {
+    //     Some(max) => println!("The maximum is configured to be {}", max),
+    //     _ => (),
+    // }
 }
 // This accepts whatever included in IpAddrKind enum
 fn route(ip_kind: &IpAddrKind) {
     println!("ip_kind: {:?}", ip_kind);
+}
+/* match, a control flow construct */
+// Works like a coin-sorting machine
+fn value_in_cents(coin: Coin) -> u8 {
+    // Should cover all possibilities otherwise it throws a compile error (how exhaustive!)
+    // Patterns are evaluated in order
+    match coin {
+        Coin::Penny => 1,
+        Coin::Nickel => 5,
+        Coin::Dime => 10,
+        Coin::Quarter(state) => {
+            println!("state quarter from: {:?} !!", state);
+            25
+        }
+    }
+}
+// Works with Option<T> as well
+fn plus_one(x: Option<i32>) -> Option<i32> {
+    match x {
+        None => None,
+        Some(i) => Some(i + 1),
+    }
 }
